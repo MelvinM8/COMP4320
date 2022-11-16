8b52d0dc28ca27177817131b8ab0d04e59821d17
Melvin Moreno <mem0282@auburn.edu>
2022-11-16 10:52:55 -0600

    Created Server side program

---
+++ Project2/Server.c
@@ -0,0 +1,12 @@
+/**
+ * @file Server.c
+ * @author Melvin Moreno, Garrison Moore, Will Thompson
+ * @brief Server side implementation of UDP File Transfer with Go-Back-N pipeline
+ * @version 0.1
+ * @date 2022-11-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// Includes
